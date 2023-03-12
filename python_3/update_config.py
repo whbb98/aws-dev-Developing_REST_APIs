dcf7f3db4a0539b8585381b2cd90a1f79bad5d70
EC2 Default User <ec2-user@ip-10-16-10-56.ec2.internal>
2023-03-12 14:25:13 +0000

    upload updated config.js to s3 bucket

--- python_3/update_config.py
+++ python_3/update_config.py
@@ -3,7 +3,7 @@
 '''
 import boto3
 S3API = boto3.client("s3", region_name="us-east-1") 
-bucket_name = "<FMI_1>"
+bucket_name = "c73321a1486368l3725768t1w936177513679-s3bucket-116k9ay7ujg38"
 
 filename = "/home/ec2-user/environment/resources/website/config.js"
 S3API.upload_file(filename, bucket_name, "config.js", ExtraArgs={'ContentType': "application/js", "CacheControl": "max-age=0"})
